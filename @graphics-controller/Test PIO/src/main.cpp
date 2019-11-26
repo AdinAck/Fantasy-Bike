f5930f41b74821f1b5343d19cff291ef425bdc29
AdinAck <adinackerman@gmail.com>
2019-11-26 13:16:58 -0800

    platformio integration

--- @graphics-controller/Test PIO/src/main.cpp
+++ @graphics-controller/Test PIO/src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 
+// the setup function runs once when you press reset or power the board
+
 void setup() {
-  // put your setup code here, to run once:
+	Serial.begin(9600);
+	Serial.println("Hello, world!");
+
+  // initialize digital pin LED_BUILTIN as an output.
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
+
+// the loop function runs over and over again forever
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  delay(1000);                       // wait for a second
+
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+  delay(1000);                       // wait for a second
+	Serial.println("Hello, world!");
 }
