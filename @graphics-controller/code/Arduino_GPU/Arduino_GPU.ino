bf553118cd06079d6b657c0122931fb31ef93895
AdinAck <adinackerman@gmail.com>
2020-02-02 13:31:52 -0800

    ...
    ...

--- @graphics-controller/code/Arduino_GPU/Arduino_GPU.ino
+++ @graphics-controller/code/Arduino_GPU/Arduino_GPU.ino
@@ -2,25 +2,23 @@
 #include <Wire.h>
 #include <SPI.h>
 
-U8G2_SSD1322_NHD_256X64_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 7, /* dc=*/ 9, /* reset=*/ 10);  // Enable U8G2_16BIT in u8g2.h
-
-#define slaveAddress 9
+U8G2_SSD1322_NHD_256X64_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 7, /* dc=*/ 9, /* reset=*/ 10);
 
 void setup() {
   u8g2.begin();
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_profont17_mf);
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
 }
 
 void loop() {
 }
 
 void receiveEvent(int howMany) {
-  while (0 < Wire.available()) { // loop through all but the last
-    int c = Wire.read(); // receive byte as a character
+  while (0 < Wire.available()) {
+    int c = Wire.read();
     if (c == 0) {
       u8g2.clearBuffer();
     }
