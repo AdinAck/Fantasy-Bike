be777d907d23b90b195aabc0cebc3643c3cab5d4
Rettdog <rettdog@gmail.com>
2020-06-20 18:01:26 -0700

    ...

--- @motherboard/code/ui.py
+++ @motherboard/code/ui.py
@@ -5,9 +5,20 @@ class Screen:
     def __init__(self):
         self.content = []
 
+    def addText(self,xpos,ypos,textSize,text):
+        self.content.append([Screen.d.drawStr, xpos, ypos, text])
+
     def addButton(self, xpos, ypos, sizex, sizey, text):
         self.content.extend([[Screen.d.drawRect, xpos, ypos, sizex, sizey],[Screen.d.drawStr, xpos, ypos, text]])
 
-def update():
+    def setContent(self,arrOfContent):
+        self.content = arrOfContent
+
+
+def update(changing):
     for i in range(len(Screen.current.content)):
-        Screen.current.content[i][0](*Screen.current.content[i][1:])
+        inputArr = Screen.current.content[i][1:]
+        for j in changing:
+            if id(inputArr[-1]) == id(j):
+                inputArr[-1] = j
+        Screen.current.content[i][0](*inputArr)
