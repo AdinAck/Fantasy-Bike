70c6de684c864a8f56e9f284f53568364cd07442
AdinAck <adinackerman@gmail.com>
2020-07-05 19:27:35 -0700

    ...

--- BMS/code/main.py
+++ BMS/code/main.py
@@ -2,19 +2,22 @@ import board
 import digitalio
 import busio
 import time
+from adafruit_mcp230xx.mcp23008 import MCP23008
 from ADS1248 import ADS1248
 from BMS import BMS
 
-
-
+# GPIO
 buz = digitalio.DigitalInOut(board.D13)
 buz.direction = digitalio.Direction.OUTPUT
 
 relay = digitalio.DigitalInOut(board.A4)
 relay.direction = digitalio.Direction.OUTPUT
 
+# SERCOM
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+i2c = busio.I2C(board.SCL, board.SDA)
 
+# ADS1248
 ADS1248.setup(spi, board.D3, board.D4)
 adc1 = ADS1248(board.D2, board.D12, 2.5)
 adc2 = ADS1248(board.D11, board.D10, 2.5)
@@ -25,7 +28,14 @@ ADS1248.wakeupAll()
 ADS1248.wregAll(2,[0x40,0x01])
 # ADS1248.selfOffsetAll()
 
-bms = BMS(ADS1248, buz, relay)
+# MCP23008
+mcp1 = MCP23008(i2c, address=0x20)
+mcp2 = MCP23008(i2c, address=0x21)
+mcp3 = MCP23008(i2c, address=0x22)
+
+# BMS
+bms = BMS(ADS1248, mcp1, mcp2, mcp3, buz, relay)
+
 
 while True:
     bms.battCheck()
